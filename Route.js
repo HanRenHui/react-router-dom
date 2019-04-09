b5044a27a2b8bc7e31aacff2e42389ae298b7d45
HanRenHui <897691781@qq.com>
2019-04-09 13:33:22 +0800

    增加模糊匹配

--- Route.js
+++ Route.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import pathToRegExp from 'path-to-regexp'
 export default class Route extends Component {
   static contextTypes = {
     location: PropTypes.object,
     history: PropTypes.object
   }
+  constructor(props) {
+    super(props)
+    let keys = []
+    this.pathReg = pathToRegExp(props.path, keys, {end: false})
+    // [id, age]
+    this.keys = keys.map(key => key.name)
+    
+    
+  }
   render() {
     let { path, component: Component } = this.props
     let { location: { pathname } } = this.context
+    // console.log(this.pathReg.test(pathname))
+    console.log(this.props.path);
+    
+    
+    let result = pathname.match(this.pathReg)
+    // let result = this.pathReg.test(pathname)
+    // console.log(this.pathReg);
+    
+    if (result) {
+      let match = {
+        path,
+        url: pathname, 
+        exact: true,
+        params: this.keys.reduce((pre, next, index) => {  
+          pre[next] = result[index+1]
+          return pre
+        }, {})
+      }
 
-    if (pathname.startsWith(path + '/') || 
-      pathname === path 
-        || path === '/') {
-      return <Component {...this.context} />
+      return <Component {...this.context} match={match}/>      
     } else {
       return null
     }
